ea701a85f58ab89412332735cfdb951a021c305d
User_Raum_174_24 <User_24@GeSa.local>
2019-05-14 15:48:35 +0200

    eventmanager used for collisions

---
+++ Assets/_memberSpaces/jan/Scripts/Reciever.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nvp.Assets.EventHandling;
+
+public class Reciever : NvpAbstractEventHandlerV2
+{
+    void Update()
+    {
+        
+    }
+
+    protected override void StartListenToEvents()
+    {
+        EventController.StartListenForEvent("HitTube".GetHashCode(), OnHitTube);
+    }
+
+    protected override void StopListenToEvents()
+    {
+        EventController.StopListenForEvent("HitTube".GetHashCode(), OnHitTube);
+    }
+
+    void OnHitTube(object s, object e)
+    {
+        Debug.Log("EventHitTubeTriggered");
+    }
+}
