f0f5acfa5aaeaf56853d2325a90de890eea2408c
User_Raum_174_18 <User_18@GeSa.local>
2019-05-14 15:39:11 +0200

    score manager added

---
+++ Assets/_memberSpaces/fynn/scripts/score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nvp.Assets.EventHandling;
+using TMPro;
+
+public class score : NvpAbstractEventHandlerV2
+{
+
+    int Score = 0;
+    public TextMeshProUGUI scoretext; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = Score.ToString("00");
+    }
+
+    void OnScored(object s, object e)
+    {
+        if (Score == 9999)
+        { } else { Score++; }
+    }
+
+    protected override void StartListenToEvents()
+    {
+        EventController.StartListenForEvent("onscored".GetHashCode(), OnScored);
+    }
+
+    protected override void StopListenToEvents()
+    {
+        EventController.StartListenForEvent("onscored".GetHashCode(), OnScored);
+    }
+}
---
+++ Assets/_memberSpaces/fynn/scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nvp.Assets.EventHandling;
+
+public class test : NvpAbstractEventHandlerV2
+{
+    // Start is called before the first frame update
+     protected override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        EventController.TriggerEvent("onscored".GetHashCode(), this, "Hello, World!");
+    }
+}
