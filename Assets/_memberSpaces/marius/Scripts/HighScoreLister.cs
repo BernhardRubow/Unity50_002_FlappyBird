4272bc1da825e124cd569519847cbf42f9a26009
User_Raum_174_19 <User_19@GeSa.local>
2019-05-14 15:50:55 +0200

    added eventController id norm and kinda worked a little bit on the highscore list

--- Assets/_memberSpaces/marius/Scripts/HighScoreLister.cs
+++ Assets/_memberSpaces/marius/Scripts/HighScoreLister.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using nvp.Assets.EventHandling;
 
-public class HighScoreLister : MonoBehaviour
+public class HighScoreLister : NvpAbstractEventHandlerV2
 {
     public TextMeshPro userScores;
     private int userScore = 13489;
@@ -17,10 +19,6 @@ public class HighScoreLister : MonoBehaviour
 
     private string numToShortString(long n)
     {
-        if (n > 1000000000000000)
-            return Mathf.Floor(n / 1000000000000000).ToString() + "Q";
-        if (n > 1000000000000)
-            return Mathf.Floor(n / 1000000000000).ToString() + "T";
         if (n > 1000000000)
             return Mathf.Floor(n / 1000000000).ToString() + "B";
         if (n > 1000000)
@@ -31,16 +29,32 @@ public class HighScoreLister : MonoBehaviour
             return n.ToString();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void OnShowHighscores(object s, object e) {
+        Debug.LogFormat("OnShowHighscores({0}, {1}) call", s.ToString(), e.ToString());
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("highscores");
         NetworkHandler.sendHTTPRequest("https://youtube.alpmann.de/gdp_unity50_flappy_bird1.php?action=top5");
+        while (asyncLoad.isDone == false) { }
+    }
+
+    // Start is called before the first frame update
+    protected override void Start() {
+        EventController.StartListenForEvent(EventIdNorm.Hash("marius", "showHighscores"), OnShowHighscores);
+        EventController.TriggerEvent(EventIdNorm.Hash("marius", "showHighscores"), this, "roflcopter");
+    }
+
+    protected override void StartListenToEvents()
+    {
+        base.StartListenToEvents();
+    }
+
+    protected override void StopListenToEvents()
+    {
+        base.StopListenToEvents();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        string text = "Your Score:\t" + userScore + "\n";
+    void Update() {
+        string text = "Your Score:\t" + numToShortString(userScore) + "\n";
         if (NetworkHandler.isDone() == false)
             text += "\n\n\nloading highscore table..\n";
         else
