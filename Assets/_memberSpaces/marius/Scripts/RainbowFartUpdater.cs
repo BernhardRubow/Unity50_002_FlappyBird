76a12db48ca8d493d0c8d339d3f77a1a579b7dd2
Gecero <27288418+Gecero@users.noreply.github.com>
2019-05-22 19:39:06 +0200

    added bird rainbow fart that follows the bird

---
+++ Assets/_memberSpaces/marius/Scripts/RainbowFartUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowFartUpdater : MonoBehaviour
+{
+    public Transform birdVisualTransform;
+    public Transform particleTransform;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+    // Update is called once per frame
+    void Update() {
+        particleTransform.eulerAngles = new Vector3(0.0f, -90.0f, birdVisualTransform.eulerAngles.z);
+        particleTransform.position = birdVisualTransform.position;
+    }
+}
