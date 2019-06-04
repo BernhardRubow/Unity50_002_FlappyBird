f8206f4cdbcecbd3b09d7a819abf9510950ef02f
User_Raum_174_25 <User_25@GeSa.local>
2019-06-04 15:29:20 +0200

    SceneManager added

---
+++ Assets/_memberSpaces/Pietro/ButtonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nvp.Assets.EventHandling;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : NvpAbstractEventHandlerV2
+{
+
+    void onStartButtonPressed(object s, object e)
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    protected override void StartListenToEvents()
+    {
+        EventController.StartListenForEvent(
+            EventIdNorm.Hash("Pietro", "onstartbutton"), onStartButtonPressed);
+    }
+
+    protected override void StopListenToEvents()
+    {
+        EventController.StopListenForEvent(
+            EventIdNorm.Hash("Pietro", "onstartbutton"), onStartButtonPressed);
+    }
+
+}
